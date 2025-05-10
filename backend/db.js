ec932026608ff15273c74e2f495b1614a3f776a8
shaghaf7 <shaghafkashif@gmail.com>
2025-05-10 19:09:13 +0530

    structured mongoose schema

---
+++ backend/db.js
@@ -0,0 +1,58 @@
+// backend/db.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+module.exports = {
+	User,
+    Account
+};
